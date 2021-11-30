cbbe67e9ad2ff6d38fcd624ddcf71ea63181cc94
Pedro Souza <54962878+pedrohdjs@users.noreply.github.com>
2021-11-29 23:06:39 -0300

    Create game.hpp

---
+++ lib/game.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Game {
+    private:
+        string msg;
+
+    public: 
+        Game();
+        void run();
+
+};
+
+#endif
