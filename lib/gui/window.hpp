a60c671fb5dfcd7d8d5c853840a98278f34345be
Math-42 <matheus.v4200@gmail.com>
2022-01-07 16:05:17 -0300

    documentação window

--- lib/gui/window.hpp
+++ lib/gui/window.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <ncurses.h>
 
 #include <string>
@@ -6,42 +7,51 @@
 
 /**
  * @brief Classe abstrata para criação de janelas.
- * 
  */
 class Window {
-    protected:
-        WINDOW* window; //Janela com o conteúdo da janela
-        /**
-         * Configuração inicial da janela, com desenho de partes estáticas.
-         * Deve ser sobreescrita em janelas mais específicas.
-         */
-        virtual void setup() = 0;
+   protected:
+    WINDOW* window;  //Janela com o conteúdo da janela
+    /**
+	 * Configuração inicial da janela, com desenho de partes estáticas.
+	 * Deve ser sobreescrita em janelas mais específicas.
+	 */
 
-        //Desenho de partes dinâmicas da janela
-        virtual void draw() = 0;
+    //Desenha de partes estaticas da janela
+    virtual void setup() = 0;
 
+    //Desenha de partes dinâmicas da janela
+    virtual void draw() = 0;
 
-    public:
-        /**
-         * Construtor da janela
-         * @param height altura da janela
-         * @param width largura da janela
-         * @param startHeigh posição X onde a janela será desenhada
-         * @param startWidth posição Y onde a janela será desenhada
-         */
-        Window(int height, int width, int startHeight, int startWidth);
-        
-		int width; //Largura da janela
-        int height; //Altura da janela
-        int startWidth;
-        int startHeight;
+   public:
+    /**
+	 * Construtor da janela
+	 * @param height altura da janela
+	 * @param width largura da janela
+	 * @param startHeigh posição X onde a janela será desenhada
+	 * @param startWidth posição Y onde a janela será desenhada
+	 */
+    Window(int height, int width, int startHeight, int startWidth);
 
-        //Destrutor da janela
-        ~Window();
+    int width;        //Largura da janela
+    int height;       //Altura da janela
+    int startWidth;   //Posição x onde a janela será desenhada
+    int startHeight;  // Posição y onde a janela será desenhada
 
-        //Faz o refresh da janela atualizando de acordo com o que está escrito no buffer.
-        void refresh();
+    /**
+	 * @brief libera a memória alocada 
+	 */
+    ~Window();
 
-        //Retorna a janela do ncurses 
-        WINDOW* getWindow();
+    /**
+	 * @brief Redesenha a janela e faz o update do buffer
+	 * 
+	 */
+    void refresh();
+
+    /**
+	 * @brief Retorna a janela do ncurses 
+	 * 
+	 * @return ponteiro para a janela
+	 */
+    WINDOW* getWindow();
 };
